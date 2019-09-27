200c1279cba4c136e22ecc6d8987540b6501b139
Ignacio Martins <ig.mar.oli@gmail.com>
2019-09-27 11:48:43 -0300

    Removed routes/web.php

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-Route::namespace('Ignacio\MercadoPago\Http\Controllers')->group(function(){
-     Route::get('test-pay', function(){
-          return view('mercadopago.pay');
-     });
-     Route::post('test-pay', 'TestController@test_pay_post')->name('mercadopago.pago');
-     Route::get('test-method', 'TestController@test_method');
-});
